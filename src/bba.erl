45ae63e6d2efbda11033d002ae6a12321f9da43a
Andrew Thompson <andrew@hijacked.us>
2018-04-06 10:33:32 -0700

    Refactor BBA

--- src/bba.erl
+++ src/bba.erl
@@ -1,52 +1,84 @@
 -module(bba).
 
--export([init/1, bv_broadcast/2]).
+-export([init/3, input/2, handle_msg/3]).
 
 -record(data, {
-          state = waiting :: waiting | done,
+          state = init :: init | waiting | done,
+          round = 0,
+          secret_key,
+          coin,
+          est :: 0 | 1,
           f :: pos_integer(),
-          witnesses = sets:new() :: sets:set({non_neg_integer(), 0 | 1}),
+          n :: pos_integer(),
+          witness = sets:new() :: sets:set({non_neg_integer(), 0 | 1}),
+          aux_witness = sets:new() :: sets:set({non_neg_integer(), 0 | 1}),
           broadcasted = sets:new() :: sets:set(0 | 1),
           bin_values = sets:new() :: set:set(0 | 1)
          }).
 
--type bval_msg() :: {bval, {pos_integer(), 0 | 1}}.
+%-type bval_msg() :: {bval, {pos_integer(), 0 | 1}}.
 
--type broadcast() :: {broadcast, bval_msg()}.
+%-type multicast() :: {multicast, bval_msg()}.
 
-init(F) ->
-    Data = #data{f=F},
-    {ok, Data}.
+init(SK, N, F) ->
+    #data{secret_key=SK, n=N, f=F}.
 
--spec bv_broadcast(#data{}, sets:set({non_neg_integer(), 0 | 1})) -> {#data{}, {send, broadcast()} | {error, not_enough_witnesses}}.
-bv_broadcast(Data=#data{f=F}, {Id, V}) ->
-    case sets:size(Data#data.witnesses) >= F+1 andalso sets:is_element(V, Data#data.broadcasted) == false of
+input(Data = #data{state=init}, BInput) ->
+    {Data#data{est = BInput}, {send, [{multicast, {bval, BInput}}]}}.
+
+handle_msg(Data, J, {bval, V}) ->
+    bval(Data, J, V);
+handle_msg(Data, J, {aux, V}) ->
+    aux(Data, J, V);
+handle_msg(Data, J, {coin, CMsg}) ->
+    %% dispatch the message to the nested coin protocol
+    ok.
+
+%-spec bv_broadcast(#data{}, sets:set({non_neg_integer(), 0 | 1})) -> {#data{}, {send, broadcast()} | {error, not_enough_witnesses}}.
+bval(Data=#data{n=N, f=F}, Id, V) ->
+    %% add to witnesses
+    Witness = sets:add_element({Id, V}, Data#data.witness),
+    WitnessCount = lists:sum([ 1 || {_, Val} <- sets:to_list(Witness), V == Val ]),
+    case WitnessCount >= F+1 andalso sets:is_element(V, Data#data.broadcasted) == false of
         true ->
-            %% broadcast
-            %% add to witnesses
             %% add to broadcasted
-            NewData = Data#data{state=waiting,
-                                witnesses=sets:add_element({Id, V}, Data#data.witnesses),
+            NewData = Data#data{witness=Witness,
                                 broadcasted=sets:add_element(V, Data#data.broadcasted)},
-            {NewData , {send, broadcast, {bval, {Id, V}}}};
+            {NewData , {send, [{multicast, {bval, V}}]}};
         false ->
-            %% add to witnesses
-            %% keep waiting
-            NewData = Data#data{state=waiting,
-                                witnesses=sets:add_element({Id, V}, Data#data.witnesses)},
-            {NewData, send, error, not_enough_witnesses}
-    end;
-bv_broadcast(Data=#data{state=waiting, f=F}, {Id, V}) ->
-    case sets:size(Data#data.witnesses) >= 2*F+1 andalso sets:is_element(V, Data#data.bin_values) == false of
-        true ->
-            %% add to witnesses?
-            %% add to binvalues
-            NewData = Data#data{state=done,
-                                witnesses=sets:add_element({Id, V}, Data#data.witnesses),
-                                bin_values=sets:add_element(V, Data#data.bin_values)},
-            {NewData , {send, done, {bval, {Id, V}}}};
-        false ->
-            %% keep waiting
-            {Data#data{state=waiting}, send, error, not_enough_witnesses}
-    end;
-bv_broadcast(Data=#data{state=done}, _) -> Data#data.bin_values.
+            case WitnessCount >= 2*F+1 of
+                true ->
+                    %% add to binvalues
+                    NewData = Data#data{witness=Witness,
+                                        bin_values=sets:add_element(V, Data#data.bin_values)},
+                    %% XXX How many times do we send AUX per round? I think just once
+                    Random = lists:nth(rand:uniform(sets:size(NewData#data.bin_values), sets:from_list(NewData#data.bin_values))),
+                    AuxSend = [{multicast, {aux, Random}}],
+
+                    %% check if we've received at least N - F AUX messages where the values in the AUX messages are member of bin_values
+                    case sets:size(sets:filter(fun({_, X}) -> sets:is_element(X, NewData#data.bin_values) end, NewData#data.aux_witness)) >= N - F of
+                        true when NewData#data.coin == undefined ->
+                            %% instanciate the common coin
+                            %% TODO need more entropy for the SID
+                            {CoinData, {send, ToSend}} = common_coin:get_coin(common_coin:init(NewData#data.secret_key, {NewData#data.round}, N, F)),
+                            {NewData#data{coin=CoinData} , {send, [ToSend|AuxSend]}};
+                        _ ->
+                            {NewData, {send, AuxSend}}
+                    end
+            end
+    end.
+
+aux(Data = #data{n=N, f=F}, Id, V) ->
+    Witness = sets:add_element({Id, V}, Data#data.aux_witness),
+    NewData = Data#data{aux_witness = Witness},
+    %% check if we've received at least N - F AUX messages where the values in the AUX messages are member of bin_values
+    case sets:size(sets:filter(fun({_, X}) -> sets:is_element(X, NewData#data.bin_values) end, NewData#data.aux_witness)) >= N - F of
+        true when NewData#data.coin == undefined ->
+            %% instanciate the common coin
+            %% TODO need more entropy for the SID
+            {CoinData, {send, ToSend}} = common_coin:get_coin(common_coin:init(NewData#data.secret_key, {NewData#data.round}, N, F)),
+            {NewData#data{coin=CoinData} , {send, ToSend}};
+        _ ->
+            {NewData, ok}
+    end.
+
