b03e3f8e3aa0d464d34eaaffba1e454bbab6a48a
Rahul Garg <vihu89@gmail.com>
2018-04-07 12:58:13 -0700

    hbbft test, WIP

--- src/hbbft.erl
+++ src/hbbft.erl
@@ -17,27 +17,27 @@
 init(SK, N, F, J) ->
     #data{secret_key=SK, n=N, f=F, j=J}.
 
-input(Data = #data{secret_key=SK, n=N, f=F}, Txn) ->
+input(Data = #data{secret_key=SK, n=N, f=_F}, Txn) ->
     Buf = queue:in(Txn, Data#data.buf),
     case queue:len(Buf) > ?BATCH_SIZE andalso Data#data.acs == undefined of
         true ->
             %% time to kick off a round
-            ACS = acs:init(SK, N, F),
+            %% ACS = acs:init(SK, N, F),
             %% compose a transaction bundle
             %%
             %% get the top b elements from buf
             List = lists:sublist(queue:to_list(Buf), ?BATCH_SIZE),
             %% pick a random B/N selection of them
             Bundle = random_n(?BATCH_SIZE div N, List),
-            EncBundle = encrypt(SK, term_to_binary(Bundle)),
+            _EncBundle = encrypt(SK, term_to_binary(Bundle)),
+            ok;
+        false ->
+            %% wait for f+1 messages?
             ok
     end.
 
 handle_msg(_, _, _) ->
     ok.
-
-
-
 
 encrypt(PK, Bin) ->
     %% generate a random AES key and IV
@@ -53,19 +53,19 @@ encrypt(PK, Bin) ->
     %% assemble a final binary packet
     <<AAD:48/binary, CipherTag:16/binary, CipherText/binary>>.
 
-get_encrypted_key(<<_IV:16/binary, EncKey:32/binary, _/binary>>) ->
-    EncKey.
-
-decrypt(SK, Key, Bin) ->
-    EncKey = get_encrypted_key(Bin),
-    case EncKey == tpke_pubkey:encrypt(tkpe_privkey:public_key(SK), Key) of
-        true ->
-            %% ok, key matches
-            <<IV:16/binary, EncKey:32/binary, Tag:16/binary, CipherText/binary>> = Bin,
-            crypto:block_decrypt(aes_gcm, Key, IV, {<<IV:16/binary, EncKey:32/binary>>, CipherText, Tag});
-        false ->
-            error
-    end.
+%% get_encrypted_key(<<_IV:16/binary, EncKey:32/binary, _/binary>>) ->
+%%     EncKey.
+%% 
+%% decrypt(SK, Key, Bin) ->
+%%     EncKey = get_encrypted_key(Bin),
+%%     case EncKey == tpke_pubkey:encrypt(tkpe_privkey:public_key(SK), Key) of
+%%         true ->
+%%             %% ok, key matches
+%%             <<IV:16/binary, EncKey:32/binary, Tag:16/binary, CipherText/binary>> = Bin,
+%%             crypto:block_decrypt(aes_gcm, Key, IV, {<<IV:16/binary, EncKey:32/binary>>, CipherText, Tag});
+%%         false ->
+%%             error
+%%     end.
 
 %% helpers
 random_n(N, List) ->
@@ -77,18 +77,40 @@ shuffle(List) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-encrypt_decrypt_test() ->
+hbbft_input_test() ->
     N = 5,
-    F = 1,
+    F = N div 4,
     dealer:start_link(N, F+1, 'SS512'),
-    {ok, PubKey, PrivateKeys} = dealer:deal(),
+    {ok, _PubKey, PrivateKeys} = dealer:deal(),
     gen_server:stop(dealer),
+    %% bunch of random txns
+    Txns = [ crypto:strong_rand_bytes(512) || _ <- lists:seq(1, N)],
+    %% some identifiers
+    Js = lists:seq(1, length(PrivateKeys)),
+    %% zipped initial states for hbbft
+    States = [init(SK, N, F, J) || {J, SK} <- lists:zip(Js, PrivateKeys)],
+    %% states bundled with txns
+    MixedList = lists:zip(States, Txns),
+    Res = lists:map(fun({State, Txn}) ->
+                            {NewState, Result} = input(State, Txn),
+                            {NewState, Result}
+                    end, MixedList),
+    io:format("Res: ~p~n", [Res]),
+    ?assert(false),
 
-    PlainText = crypto:strong_rand_bytes(24),
-    Enc = encrypt(Pubkey, PlainText),
-    EncKey = get_encrypted_key(Enc),
-    DecKey = tkpe_pubkey:combine_shares([ tpke_privkey:decrypt_share(SK, EncKey) || SK <- PrivateKeys]),
     ok.
 
+%% encrypt_decrypt_test() ->
+%%     N = 5,
+%%     F = 1,
+%%     dealer:start_link(N, F+1, 'SS512'),
+%%     {ok, PubKey, PrivateKeys} = dealer:deal(),
+%%     gen_server:stop(dealer),
+%% 
+%%     PlainText = crypto:strong_rand_bytes(24),
+%%     Enc = encrypt(PubKey, PlainText),
+%%     EncKey = get_encrypted_key(Enc),
+%%     DecKey = tkpe_pubkey:combine_shares([ tpke_privkey:decrypt_share(SK, EncKey) || SK <- PrivateKeys]),
+%%     ok.
 -endif.
 
