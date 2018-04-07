e09bc80e6570e12c98af07e87403df35a96efee7
Andrew Thompson <andrew@hijacked.us>
2018-04-07 16:23:27 -0700

    Encryption/decryption works

--- src/hbbft.erl
+++ src/hbbft.erl
@@ -44,28 +44,31 @@ encrypt(PK, Bin) ->
     Key = crypto:strong_rand_bytes(32),
     IV = crypto:strong_rand_bytes(16),
     %% encrypt that random AES key with the HBBFT replica set's public key
-    %% TODO the result of the encryption is a 3-tuple that contains 2 PBC Elements and a 32 byte binary
+    %% 
+    %% the result of the encryption is a 3-tuple that contains 2 PBC Elements and a 32 byte binary
     %% we need to encode all this crap into a binary value that we can unpack again sanely
-    EncKey = tpke_pubkey:encrypt(PK, Key),
+    {U, V, W} = tpke_pubkey:encrypt(PK, Key),
+    UBin = erlang_pbc:element_to_binary(U),
+    WBin = erlang_pbc:element_to_binary(W),
+    EncKey = <<(byte_size(UBin)):8/integer-unsigned, UBin/binary, V:32/binary, (byte_size(WBin)):8/integer-unsigned, WBin/binary>>,
+    io:format("Enc key is ~p bytes (~p ~p ~p)~n", [byte_size(EncKey), byte_size(UBin), byte_size(V), byte_size(WBin)]),
     %% encrypt the bundle with AES-GCM and put the IV and the encrypted key in the Additional Authenticated Data (AAD)
-    AAD = <<IV:16/binary, EncKey:32/binary>>,
+    AAD = <<IV:16/binary, (byte_size(EncKey)):16/integer-unsigned, EncKey/binary>>,
     {CipherText, CipherTag} = crypto:block_encrypt(aes_gcm, Key, IV, {AAD, Bin}),
     %% assemble a final binary packet
-    <<AAD:48/binary, CipherTag:16/binary, CipherText/binary>>.
+    <<AAD/binary, CipherTag:16/binary, CipherText/binary>>.
 
-get_encrypted_key(<<_IV:16/binary, EncKey:32/binary, _/binary>>) ->
-    EncKey.
+get_encrypted_key(SK, <<_IV:16/binary, EncKeySize:16/integer-unsigned, EncKey:EncKeySize/binary, _/binary>>) ->
+    <<USize:8/integer-unsigned, UBin:USize/binary, V:32/binary, WSize:8/integer-unsigned, WBin:WSize/binary>> = EncKey,
+    %% XXX we don't have a great way to deserialize the elements yet, this is a hack
+    Ugh = tpke_pubkey:hash_message(tpke_privkey:public_key(SK), <<"ugh">>),
+    U = erlang_pbc:binary_to_element(Ugh, UBin),
+    W = erlang_pbc:binary_to_element(Ugh, WBin),
+    {U, V, W}.
 
-decrypt(SK, Key, Bin) ->
-    EncKey = get_encrypted_key(Bin),
-    case EncKey == tpke_pubkey:encrypt(tkpe_privkey:public_key(SK), Key) of
-        true ->
-            %% ok, key matches
-            <<IV:16/binary, EncKey:32/binary, Tag:16/binary, CipherText/binary>> = Bin,
-            crypto:block_decrypt(aes_gcm, Key, IV, {<<IV:16/binary, EncKey:32/binary>>, CipherText, Tag});
-        false ->
-            error
-    end.
+decrypt(Key, Bin) ->
+    <<IV:16/binary, EncKeySize:16/integer-unsigned, EncKey:EncKeySize/binary, Tag:16/binary, CipherText/binary>> = Bin,
+    crypto:block_decrypt(aes_gcm, Key, IV, {<<IV:16/binary, EncKeySize:16/integer-unsigned, EncKey:(EncKeySize)/binary>>, CipherText, Tag}).
 
 %% helpers
 random_n(N, List) ->
@@ -85,9 +88,10 @@ encrypt_decrypt_test() ->
     gen_server:stop(dealer),
 
     PlainText = crypto:strong_rand_bytes(24),
-    Enc = encrypt(Pubkey, PlainText),
-    EncKey = get_encrypted_key(Enc),
-    DecKey = tkpe_pubkey:combine_shares([ tpke_privkey:decrypt_share(SK, EncKey) || SK <- PrivateKeys]),
+    Enc = encrypt(PubKey, PlainText),
+    EncKey = get_encrypted_key(hd(PrivateKeys), Enc),
+    DecKey = tpke_pubkey:combine_shares(PubKey, EncKey, [ tpke_privkey:decrypt_share(SK, EncKey) || SK <- PrivateKeys]),
+    ?assertEqual(PlainText, decrypt(DecKey, Enc)),
     ok.
 
 -endif.
