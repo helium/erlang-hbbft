f0c90d8a0a3d74a5f2bc94f26b29bba1e29d022c
Andrew Thompson <andrew@hijacked.us>
2018-07-17 15:00:18 -0700

    Fix a nasty bug in ACS that loses BBA results

--- test/hbbft_acs_SUITE.erl
+++ test/hbbft_acs_SUITE.erl
@@ -5,12 +5,14 @@
 
 -export([all/0, init_per_testcase/2, end_per_testcase/2]).
 -export([
-         init_test/1
+         init_test/1,
+         one_dead/1
         ]).
 
 all() ->
     [
-     init_test
+     init_test,
+     one_dead
     ].
 
 init_per_testcase(_, Config) ->
@@ -41,6 +43,28 @@ init_test(Config) ->
     ConvergedResultsList = sets:to_list(ConvergedResults),
     ct:pal("ConvergedResultsList: ~p~n", [ConvergedResultsList]),
     ?assertEqual(N, sets:size(ConvergedResults)),
+    DistinctResults = sets:from_list([BVal || {result, {_, BVal}} <- sets:to_list(ConvergedResults)]),
+    ?assertEqual(1, sets:size(DistinctResults)),
+    ?assert(sets:is_subset(sets:from_list([ X || {_, X} <- lists:flatten(sets:to_list(DistinctResults))]), sets:from_list(Msgs))),
+    ok.
+
+one_dead(Config) ->
+    N = proplists:get_value(n, Config),
+    F = proplists:get_value(f, Config),
+    Module = proplists:get_value(module, Config),
+    PrivateKeys = proplists:get_value(privatekeys, Config),
+    Msgs = [ crypto:strong_rand_bytes(512) || _ <- lists:seq(1, N)],
+    StatesWithId = [{J, hbbft_acs:init(Sk, N, F, J)} || {J, Sk} <- lists:zip(lists:seq(0, N - 1), PrivateKeys)],
+    MixedList = lists:zip(Msgs, StatesWithId),
+    Res = lists:map(fun({Msg, {J, State}}) ->
+                            {NewState, Result} = hbbft_acs:input(State, Msg),
+                            {{J, NewState}, {J, Result}}
+                    end, tl(MixedList)),
+    {NewStates, Results} = lists:unzip(Res),
+    {_, ConvergedResults} = hbbft_test_utils:do_send_outer(Module, Results, NewStates, sets:new()),
+    ConvergedResultsList = sets:to_list(ConvergedResults),
+    ct:pal("ConvergedResultsList: ~p~n", [ConvergedResultsList]),
+    ?assertEqual(N - 1, sets:size(ConvergedResults)),
     DistinctResults = sets:from_list([BVal || {result, {_, BVal}} <- sets:to_list(ConvergedResults)]),
     ?assertEqual(1, sets:size(DistinctResults)),
     ?assert(sets:is_subset(sets:from_list([ X || {_, X} <- lists:flatten(sets:to_list(DistinctResults))]), sets:from_list(Msgs))),
