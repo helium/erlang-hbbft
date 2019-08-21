317c0ea18a61d82ee39428ed5fba123fa526acb6
Andrew Thompson <andrew@hijacked.us>
2019-08-21 08:57:50 -0700

    Don't discard Ready messages when we're in the init state of RBC (#47)

--- test/hbbft_relcast_worker.erl
+++ test/hbbft_relcast_worker.erl
@@ -44,7 +44,9 @@ init(Args) ->
     ID = proplists:get_value(id, Args),
     DataDir = proplists:get_value(data_dir, Args),
     Members = lists:seq(1, N),
-    {ok, Relcast} = relcast:start(ID, Members, hbbft_handler, Args, [{data_dir, DataDir ++ integer_to_list(ID)}]),
+    {ok, Relcast} = relcast:start(ID, Members, hbbft_handler, Args,
+                                  [{create, true},
+                                   {data_dir, DataDir ++ integer_to_list(ID)}]),
     Peers = maps:from_list([{I, undefined} || I <- Members, I /= ID ]),
     {ok, do_send(#state{relcast=Relcast, id=ID, peers=Peers})}.
 
