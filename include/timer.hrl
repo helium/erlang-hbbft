8d545605ac3f3e69abd7d67bff4454d9d3fb03b9
Evan Vigil-McClanahan <evan@helium.com>
2020-04-21 11:16:45 -0700

    lower threshold

--- include/timer.hrl
+++ include/timer.hrl
@@ -4,7 +4,7 @@
             __Ret = (Expr),
             __End = erlang:monotonic_time(millisecond),
             case  __End - __Start of
-                __Total when __Total > 10 ->
+                __Total when __Total > 3 ->
                     lager:info("~p took ~pms", [Mark, __Total]);
                 _ -> ok
             end,
