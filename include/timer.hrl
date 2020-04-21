c4984e705197d07b390342173f867a832af99f8c
Evan Vigil-McClanahan <evan@helium.com>
2020-04-21 10:58:44 -0700

    missing file

---
+++ include/timer.hrl
@@ -0,0 +1,12 @@
+-define(timer(Mark, Expr),
+        begin
+            __Start = erlang:monotonic_time(millisecond),
+            __Ret = (Expr),
+            __End = erlang:monotonic_time(millisecond),
+            case  __End - __Start of
+                __Total when __Total > 10 ->
+                    lager:info("~p took ~pms", [Mark, __Total]);
+                _ -> ok
+            end,
+            __Ret
+        end).
